cbbe2cd1d7b0d053cfd56b6703c657dae6499fde
Maxwell <maxwell@layer8games.com>
2021-12-15 13:13:03 -0700

    began work on the app. Finished a few sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-function App() {
+import Header from './components/Layout/Header'
+
+const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
