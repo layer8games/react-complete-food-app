a7d0a296f492043924ed3cf3a9da2b76b186c1e3
Maxwell <maxwell@layer8games.com>
2022-01-03 18:43:58 -0800

    Worked up to section 141

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {React, useState} from 'react'
 import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 const App = () => {
+  const [isCartVisible, setIsCartVisible] = useState(false)
+
+  const showCartHandler = () => {
+    setIsCartVisible(true);
+  }
+
+  const hideCartHandler = () => {
+    setIsCartVisible(false);
+  }
+
   return (
     <>
-      <Header />
+      {isCartVisible && <Cart />}
+      <Header onShowCart={showCartHandler} onHideCart={hideCartHandler} />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
