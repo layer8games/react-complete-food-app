d3f37e8c39c73d9457abbff6bc281db900a94866
Maxwell <maxwell@layer8games.com>
2022-03-02 20:00:14 -0800

    I tried to get the api to work myself. I couldn't quite crack it. I'll check out the video next

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,66 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState();
+
+  const fetchMealsHandler = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://react-http-sandbox-492c0-default-rtdb.firebaseio.com/Meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      let count = 0;
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          key: count,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+        count += 1;
+      }
+
+      setMeals(loadedMeals);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, [fetchMealsHandler]);
+
+  let content = <p>SUCK IT</p>;
+
+  if (meals.length > 0) {
+    console.log(meals);
+    content = meals.map((meal) => (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
+  }
 
   return (
     <section className={classes.meals}>
       <ul>
-        <Card>{mealsList}</Card>
+        <Card>{content}</Card>
       </ul>
     </section>
   );
